9b0d611a82982eca94fd92a01df62ffd96e3c07d
Vivance <114446365+vivanceA@users.noreply.github.com>
2023-07-07 15:19:08 +0000

    back-ends

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 """
-    python script that exports data in the CSV format
+    python script that exports data in the JSON format
 """
-import csv
 import json
 import requests
 from sys import argv
-
 
 if __name__ == "__main__":
     """
@@ -42,10 +40,17 @@ if __name__ == "__main__":
     for dictionary in user_todos:
         tasks.update({dictionary.get("title"): dictionary.get("completed")})
 
+    task_list = []
+    for k, v in tasks.items():
+        task_list.append({
+            "task": k,
+            "completed": v,
+            "username": username
+        })
+
+    json_to_dump = {argv[1]: task_list}
     """
-        export to CSV
+        export to JSON
     """
-    with open('{}.csv'.format(argv[1]), mode='w') as file:
-        file_editor = csv.writer(file, delimiter=',', quoting=csv.QUOTE_ALL)
-        for k, v in tasks.items():
-            file_editor.writerow([argv[1], username, v, k])
+    with open('{}.json'.format(argv[1]), mode='w') as file:
+        json.dump(json_to_dump, file)
